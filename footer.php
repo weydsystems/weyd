f4fc34ce75d6d56d54aa5301399431c5a2f7bbc3
Weyd Nigeria <ekemini@weyd.com.ng>
2017-07-16 17:27:41 +0100

    update has been made
    update has been made

--- footer.php
+++ footer.php
@@ -1,11 +1,65 @@
 <div class="footer" id="footer">
 <div class="container">
-<ul class=" pull-left navbar-link footer-nav">
-<li><a href="index"> Home </a> <a href="about-us"> About us </a><a href="how-it-works">How it works</a> <a href="terms-conditions"> Terms and
-Conditions </a> <a href="contact">Contact</a>
+
+
+<div class="weyd-l-info">
+<h1>Categories</h1>
+
+<ul  class=" pull-left navbar-link footer-nav">
+<li> <a href="mobiles">Mobiles</a></li>
+<li> <a href="electronics-and-appliances">Electronics & Appliances</a></li>
+
+<li> <a href="real-estate">Real Estate</a></li>
+
+<li> <a href="health-and-beauty">Health & Beauty</a></li>
+<li> <a href="hobby-sport-and-kid">Hobby sport and kid</a></li>
+<li> <a href="animals-and-pets">Animal & pets</a></li>
+
+<li> <a href="fashion">Fashion</a></li>
+<li> <a href="cars">Car & vehicles</a></li>
+<li> <a href="add-category">Others</a></li>
+
+
+
+
 </ul>
-<ul class=" pull-right navbar-link footer-nav">
-<li> &copy; <?php echo date("Y");?> Weyd.com.ng</li>
+</div>
+<div class="weyd-r-info">
+<h1>RESOURCES</h1>
+<ul  class=" pull-left navbar-link footer-nav">
+<li> <a href="https://facebook.com/weydng"><i class="fa fa-facebook" style="color:#1B75A8 ; font-size:20px;"></i></a></li>
+
+<li> <a href="https://instagram.com/weyd_ng"><i class="fa fa-instagram" style="color:#586871 ; font-size:20px;"></i></a></li>
+
+
+
+
+
+
+
 </ul>
+</div>
+
+<div class="weyd-c-info">
+  <h1>GET TO KNOW US</h1>
+  <ul  class=" pull-left navbar-link footer-nav">
+  <li> <a href="how-it-works">How it works</a></li>
+  <li> <a href="about">About</a></li>
+  <li> <a href="terms-conditions">Terms and Condition</a></li>
+  <li> <a href="contact">Contact</a></li>
+
+
+
+
+
+
+
+  </ul>
+
+</div>
+<div class="reserved">
+<a href="terms-conditions.php">Terms of use</a> 2016- <?php echo date("Y");?> <b>weyd classifieds</b> All Rights Reserved.
+
+</div>
 </div>
 </div>
